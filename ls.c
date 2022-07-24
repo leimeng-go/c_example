8156a639353a8e97aae00913add714b88b6090ca
meng lei <1955854667@qq.com>
2022-07-24 19:14:07 +0800

    添加apue.h

--- ls.c
+++ ls.c
@@ -1,8 +1,12 @@
 #include "apue.h"
-#include <stdio.h>
+#include <dirent.h>
 
 int main(int argc,char *argv[])
 {
     DIR *dp;
+    struct dirent *dirp;
+    
+    if (argc!=2)
+      err_quit("usage: ls directory_name");
 
 }
