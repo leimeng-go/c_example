9e64f025160dc28383df20d9045c946ed70fc772
meng lei <1955854667@qq.com>
2022-07-24 20:53:12 +0800

    添加ls.c

--- ls.c
+++ ls.c
@@ -8,5 +8,10 @@ int main(int argc,char *argv[])
     
     if (argc!=2)
       err_quit("usage: ls directory_name");
-
+    if ((dp=opendir(argv[1]))==NULL)
+      err_sys("can't open %s",argv[1]);
+    while ((dirp=readdir(dp))!=NULL)
+      printf("%s\n",dirp->d_name);
+    closedir(dp);
+    exit(0);    
 }
